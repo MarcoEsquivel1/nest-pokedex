99743077cb12a470a6480e2ecc5487f5cb468921
MarcoEsquivel <marco.esquivel@catolica.edu.sv>
2022-12-23 17:52:00 -0600

    Crud nestjs

---
+++ src/common/pipes/parse-mongo-id/parse-mongo-id.pipe.ts
@@ -0,0 +1,11 @@
+import { ArgumentMetadata, Injectable, PipeTransform, BadRequestException } from '@nestjs/common';
+import { isValidObjectId } from 'mongoose';
+
+@Injectable()
+export class ParseMongoIdPipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata) {
+    if (!isValidObjectId(value))
+      throw new BadRequestException(`${value} is not a valid MongoID`)
+    return value;
+  }
+}
